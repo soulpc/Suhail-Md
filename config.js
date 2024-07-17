b6f83e370d6047ebef44ed7b1e8ccfe9983064a1
Soul pc <119080077+soulpc@users.noreply.github.com>
2024-07-17 14:15:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ global.caption = process.env.CAPTION || global.caption || "ᴘᴏᴡᴇʀᴇᴅ 
 
 global.devs = "919747358233" // Developer Contact
 global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "null";
-global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "919747358233";
+global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "919747358233 91 70122 32015";
 
 
 
